27e4eecc84dbac4bd0de0d6a318c7017e083b5fb
zaferatakli <zatakli@gmail.com>
2025-03-08 11:50:07 +1100

    with OOP & Without OOP version update

--- src/WithOOP/Triangle.java
+++ src/WithOOP/Triangle.java
@@ -10,31 +10,6 @@ public class Triangle {
         this.side2 = side2;
         this.side3 = side3;
     }
-
-    public int getSide1() {
-        return side1;
-    }
-
-    public void setSide1(int side1) {
-        this.side1 = side1;
-    }
-
-    public int getSide2() {
-        return side2;
-    }
-
-    public void setSide2(int side2) {
-        this.side2 = side2;
-    }
-
-    public int getSide3() {
-        return side3;
-    }
-
-    public void setSide3(int side3) {
-        this.side3 = side3;
-    }
-
     public boolean isValidTriangle() {
         return (side1 + side2 > side3) && (side3 > Math.abs(side1 - side2));
     }
